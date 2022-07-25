76d0b9db7dec8d59cfb7c4adb916ebc6aed43cc5
keerthana1801 <keerthana9achar@gmail.com>
2022-07-25 16:31:28 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 1;
 
   void _incrementCounter() {
     DatabaseReference _testRef = FirebaseDatabase.instance.ref().child("test");
